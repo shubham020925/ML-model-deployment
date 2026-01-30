de70519fe70f9c0bd7bd614a27015e184c1153f8
shubham020925 <161818941+shubham020925@users.noreply.github.com>
2026-01-30 12:44:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
 import streamlit as st
 import pandas as pd
-import pickle
+import pickle # changed from joblib to pickle
 import numpy as np
 import os
 
-st.set_page_config(page_title="Iris Classifier",page_icon="🌸") # flower = windows + dot
+#--- PAGE CONFIG---
+st.set_page_config(page_title="Iris Classifier", page_icon="")
 
+#--- LOAD THE TRAINED MODEL
 @st.cache_resource
 def load_model():
-
-  model_path="iris_model.pkl"
+  #Update the path to look for the pkl file
+  model_path= "iris_model.pkl"
   if os.path.exists(model_path):
-    with open(model_path,'rb')as file:
+    with open(model_path, 'rb') as file:
       return pickle.load(file)
   else:
-    st.error(f"Model file'{Model_path}' not found!")
+    st.error("Model file '(model_path)' not found!")
     return None
-model=load_model()
 
-st.title("🌸 Iris Species Predictor")
-st.markdown(""" This app uses a ** logistic Regression** model to predict the species of an Iris flower based on its physical measurement.""")
-st.sidebar.header("Input Floral Features")
+model = load_model()
